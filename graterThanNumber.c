9c1756e09dec327f686a09d7bb02ddeaedcc2873
PPProsenjite <prosenjitpappu03455@gmial.com>
2024-11-20 16:05:40 +0600

    greater thaan number with three number

---
+++ graterThanNumber.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+long largeNumber(int num1, int num2, int num3){
+    if (num1 > num2 ){
+        if (num1 > num3){
+            printf(" %d is gater than number %d and %d", num1, num2, num3);
+        }
+        else
+        printf(" %d is gater than number %d and %d", num3, num1, num2);
+    }
+    else if( num2 > num3){
+        printf(" %d is gater than number %d and %d", num2, num1, num3);
+    }
+    else {
+        printf(" %d is gater than number %d and %d", num3, num1, num2);
+    }
+    return 0;
+    
+}
+int main(){
+    int num1, num2, num3 ;
+    printf("Enter three number:  ");
+    scanf("%d %d %d",&num1 ,&num2 ,&num3);
+    largeNumber(num1, num2, num3);
+}
--- primeWithRange.c
+++ primeWithRange.c
@@ -6,13 +6,13 @@ long primeCheck(int start, int end){
         count = 0; //counter initialization 
         //loop for the possible divisors.
         for( j = 2; j <= i/2; j++){
-            //if the devisor is found.
+            //if the divisor is found.
             if(i % j == 0){
                 count++;
                 break;
             }
         }
-        //if no devisors found and the number is not 1 than prime 
+        //if no divisors found and the number is not 1 than prime 
         //1 is not a prime because it has only one divisor
         if(count == 0 && i != 1){
             printf("%d, ", i);
